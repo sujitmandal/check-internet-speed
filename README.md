7177abbcf423faaa22478c13b139b47d46545f71
sujitmandal <mandals974@gmail.com>
2024-10-24 12:15:01 +0530

    creted check_internet_speed scipt 	new file:   MANIFEST.in 	modified:   README.md 	new file:   check_internet_speed/check_internet_speed.py 	new file:   requirements.txt 	new file:   setup.py 	new file:   tox.ini

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # check_internet_speed
 Check Internet Speed
+
+
+
+
+```
+aws s3 bucket cli
+
+upload, download, check file availability, and get all available list from AWS s3 bucket.
+```
+
+
+## Package Installation : 
+```
+pip install aws-s3-cli
+```
+[Pypi Package Link](https://pypi.org/project/aws-s3-cli/)
+
+
+## How to import the module:
+```python
+from check_internet_speed.check_internet_speed import check_internet_speed
+
+status = check_internet_speed()
+
+print(status)
+```
+
+
+
+
+
+## License:
+MIT Licensed
+
+## Author:
+Sujit Mandal
+
+[GitHub](https://github.com/sujitmandal)
+
+[PyPi](https://pypi.org/user/sujitmandal/)
+
+[LinkedIn](https://www.linkedin.com/in/sujit-mandal-91215013a/)
---
+++ setup.py
@@ -0,0 +1,36 @@
+__author__ = 'Sujit Mandal'
+#Date :24-10-2024
+from setuptools import setup 
+
+def readme():
+    with open('README.md') as files:
+        README = files.read()
+
+    return(README)
+
+setup(
+    name = 'check-internet-speed',
+    version = '0.0.1',
+    description = "checking internet upload and download speed.",
+    long_description = readme(),
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/sujitmandal/check-internet-speed',
+    author = 'Sujit Mandal',
+    author_email = 'mandals974@gmail.com',
+    license = 'MIT',
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+
+
+    packages = ['check_internet_speed'],
+    include_package_data = True,
+)
